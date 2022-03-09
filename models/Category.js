0828c0fb9621df6af011623e85877633bd18bb40
Aiden Brennan <Aiden.c.b@hotmail.com>
2022-03-09 21:25:32 +1100

    Add files via upload

---
+++ models/Category.js
@@ -0,0 +1,25 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Category extends Model {}
+
+Category.init(
+  { id:
+    {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  category_name: {
+    sequelize: sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'category',
+  }
+},
+);
+
+module.exports = Category;
